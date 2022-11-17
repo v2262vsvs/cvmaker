45d2e5bb95bd1800ae3b60b16d28331b44416814
Gvich <wert130202@gmail.com>
2022-11-18 00:33:12 +0200

    fix error in deploy

--- components/pageSections/Footer.tsx
+++ components/pageSections/Footer.tsx
@@ -11,13 +11,13 @@ const Footer = () => {
             <footer className="p-4 bg-white w-screen sm:p-6">
                 <div className="sm:flex sm:items-center sm:justify-between">
                 <span className="text-sm text-gray-500 sm:text-center ">
-                    © 2022 <a href="https://cvmaker-gamma.vercel.app/" target="_blank" rel="noreferrer" className="hover:underline">{t('CV - online constructor')}</a>.   {t('All Rights Reserved.')}
+                    © 2022 <a href="https://cvmaker-gamma.vercel.app/" rel="noreferrer" className="hover:underline">{t('CV - online constructor')}</a>.   {t('All Rights Reserved.')}
                 </span>
                     <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-                        <a href="https://www.figma.com/file/pVTfLw30EJGdAV1XgX2FIl/CV-project?t=TO5w7dPxEKaBqVZR-1" className=" mt-0.5 animation-footer" target="_blank">
+                        <a href="https://www.figma.com/file/pVTfLw30EJGdAV1XgX2FIl/CV-project?t=TO5w7dPxEKaBqVZR-1" className=" mt-0.5 animation-footer">
                             <img src='/figma.svg' alt='figma'/>
                         </a>
-                        <a href="https://github.com/v2262vsvs/cvmaker.git" className={'animation-footer'} target="_blank">
+                        <a href="https://github.com/v2262vsvs/cvmaker.git" className={'animation-footer'} >
                             <img src='/github.svg' alt='github'/>
                         </a>
                     </div>
